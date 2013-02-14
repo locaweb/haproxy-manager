628f1815b73ebe02668d51c86d1f7b5c5cbf23a5
PotHix <pothix@pothix.com>
2013-02-14 20:08:56 -0200

    Adding some methods for the server. WIP

--- src/haproxy_manager/server.py
+++ src/haproxy_manager/server.py
@@ -1,5 +1,9 @@
-from flask import Flask
+import json
+
+from flask import Flask, request
 app = Flask(__name__)
+
+from haproxy_manager.manager import Manager
 
 
 @app.route("/")
@@ -7,5 +11,25 @@ def info():
     return "Haproxy manager working"
 
 
+@app.route("/list")
+def list():
+    manager = Manager("tests/output/")
+    return json.dumps(manager.list())
+
+
+@app.route("/<name>", methods=["GET", "PUT", "DELETE"])
+def conf(name):
+    manager = Manager("tests/output/")
+
+    if request.method == 'GET':
+        return json.dumps(manager.get(name))
+
+    elif request.method == 'PUT':
+        return json.dumps(manager.update(name))
+
+    elif request.method == 'DELETE':
+        return json.dumps(manager.delete(name))
+
+
 def run():
-    app.run()
+    app.run(debug=True)
